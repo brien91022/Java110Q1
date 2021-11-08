d9a861cc67e47bb200786a8f94e7fad5fd280c5e
brien91022 <92264279+brien91022@users.noreply.github.com>
2021-11-08 19:29:53 +0800

    Update class03.java

--- class03.java
+++ class03.java
@@ -1,19 +1,60 @@
-class Data{
-	private String name;
-	//private Test score;
+class Testx{
+	int english;
+	int math;
+	String x;
+		
+	void namex (String x) {
+		System.out.println("name : " + x);
+	}
 	
-	void Test(int eng, int m) {
-		
+	void scoreA(int eng,int m){
+		english = eng;
+		math = m;
+		System.out.println("english = " + eng);
+		System.out.println("math = " + m);
 	}
-	void 
+	
+	void avg(int eng,int m) {
+		english = eng;
+		math = m;
+		System.out.println("avg = "+ (eng + m)/2.0);
+	}
+}	
+
+class Testy{	
+	int english;
+	int math;
+	String y;
+	
+	void namey (String y) {
+		System.out.println("name : " + y);;
+	}
+	
+	void scoreB(int eng,int m) {
+		english = eng;
+		math = m;		
+		System.out.println("english = " + eng);
+		System.out.println("math = " + m);
+	}
+	
+	void avg(int eng,int m) {
+		english = eng;
+		math = m;
+		System.out.println("avg = "+(eng + m)/2.0);
+	}
 }
-public class class03 {
 
+public class Class03 {
+	
 	public static void main(String[] args) {
-		Data a = new Data();
-		a.Test(85,92);
+		Testx a = new Testx();
+		Testy b = new Testy();
+		a.namex("Annie");
+		a.scoreA(85,92);
+		a.avg(85,92);
 		
-
+		b.namey("Brian");
+		b.scoreB(77,56);
+		b.avg(77, 56);
 	}
-
 }
