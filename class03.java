86140dd44c84cbbe7351c0589f889e24399bc08b
brien91022 <92264279+brien91022@users.noreply.github.com>
2021-11-08 11:11:11 +0800

    Create class03.java

---
+++ class03.java
@@ -0,0 +1,19 @@
+class Data{
+	private String name;
+	//private Test score;
+	
+	void Test(int eng, int m) {
+		
+	}
+	void 
+}
+public class class03 {
+
+	public static void main(String[] args) {
+		Data a = new Data();
+		a.Test(85,92);
+		
+
+	}
+
+}
